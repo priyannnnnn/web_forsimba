41270456423fcad7eb74b396cfcc5c5b2251e39d
priyannnnnn <supriyanto031003@gmail.com>
2023-09-10 23:20:14 +0700

    web_forsimba

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import SignUp from './components/pages/SignUp';
+import Products from './components/pages/Products';
+
+//app
+
+function App() {
+  return (
+   <>
+   <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/services' exact component={Services} />
+        <Route path='/products' exact component={Products} />
+        <Route path='/sign-up' exact component={SignUp} />
+      </Switch>
+   </Router>
+   </>
+  );
+}
+
+export default App;
